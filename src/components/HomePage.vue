75ad69a1cb123a79b9dd8d1fb2043474c82a7a59
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-02-23 13:13:45 +0100

    Updated

---
+++ src/components/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>{{ page }}</h1>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HomePage",
+    data() {
+        return {
+            page: "Home Page",
+            msg: String
+        }
+    }
+}
+</script>
