ffe6c2c01c77a88a3bd90094632c7e03275d1add
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-03-12 16:54:44 +0100

    Updated

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,17 +1,38 @@
 <template>
     <div>
-        <h1>{{ page }}</h1>
+        <h1>Counter: {{ count }}</h1>
+        <input type="text" name="count" id="myCount"><br>
+        <button type="submit">Submit</button>
+
+        <br>
+        <button @click="increment">Increment</button>
+        <button @click="decrement">Decrement</button>
+        <button @click="reset">Reset</button>
     </div>
 </template>
+
 
 <script>
 export default {
     name: "HomePage",
     data() {
         return {
-            page: "Home Page",
-            msg: String
+            count: 0,
         }
-    }
+    },
+    props: {
+        msg: String,
+    },
+    methods: {
+        increment() {
+            this.count++
+        },
+        decrement() {
+            this.count--
+        },
+        reset() {
+            this.count = 0
+        }, 
+    },
 }
 </script>
